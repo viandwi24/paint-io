122ee93e338604f9e7dde531740c40667a62fb29
viandwi24 <fiandwi0424@gmail.com>
2020-05-12 15:01:01 +0700

    update server url

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        serverUrl: 'http://localhost:3000',
+        serverUrl: 'https://paint-io-server.herokuapp.com',
         player: { name : null },
         socket: null,
         currentScreen: 'home',
